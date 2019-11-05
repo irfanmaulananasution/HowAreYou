13c71295f653e2c0442854434c3c32d132e3a0dd
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2019-11-05 21:40:52 +0700

    [GREEN]adding design

--- landingPage/forms.py
+++ landingPage/forms.py
@@ -2,8 +2,13 @@ from django import forms
 from .models import StatusModels
 
 class StatusForms(forms.ModelForm):
-	status = forms.CharField()
+	status = forms.CharField(widget=forms.TextInput(attrs={
+        "class" : "form-control",
+		"placeholder" : "Gimana kabarmu hari ini?",
+		"required" : True
+	}))
 	
 	class Meta:
 		model = StatusModels
 		fields = '__all__'
+		
